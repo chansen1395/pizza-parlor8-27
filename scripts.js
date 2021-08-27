10fb2147fa31d3733228fcf1fb40d1f45c61acac
Connor Hansen <chansen13@georgefox.edu>
2021-08-27 09:05:17 -0700

    determine the price based on the size.

--- scripts.js
+++ scripts.js
@@ -2,13 +2,19 @@ function Pizza(size) {
   this.size = size;
 }
 
-let price = 0;
 Pizza.prototype.getSizePrice = function () {
+  let price = 0;
   if (this.size === "S") {
   price += 5;
   return price;
-  }
+  } else if (this.size === "M") {
+  price += 10;
+  return price;
+  } else if (this.size === "L") {
+  price += 15;
+  return price;
+  } else {return price;}
 }
 
-const pizza = new Pizza("S");
+const pizza = new Pizza("X");
 pizza.getSizePrice();
